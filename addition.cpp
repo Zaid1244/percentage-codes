e41ee0e5c7f8d3d6a3e91f2d02b99adffb830def
Zaid1244 <mohdzaidabbaszaidi@gmail.com>
2025-06-19 17:52:37 +0530

    addition

---
+++ addition.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+
+    // Input two numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Perform addition
+    sum = num1 + num2;
+
+    // Output result
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
